c1d5a7d27099aaa32d7bbc1afbdccd6d57cea9a8
Aksel Sloan <akselsloan@gmail.com>
2025-02-13 15:21:39 -1000

    Saving images, improved contrast.

--- audio_to_spectro.py
+++ audio_to_spectro.py
@@ -3,13 +3,39 @@ from scipy import signal
 from scipy.io import wavfile
 import numpy as np
 
-sample_rate, data = wavfile.read('/home/gpu_enjoyer/FKW_tools/Fake whale sounds.wav') # Skips metadata
+# Reference: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.spectrogram.html
+
+###################################################################
+# VARIABLES THAT CHANGE:
+wave_file_path = 'short_false_killer_whale_sounds.wav'
+desired_channel = 3             # Which channel do you want?
+down_sample_tf = True           # Do you want to down sample? 
+down_sample_rate = 10           # How muich down sampling?
+
+###################################################################
+
+
+sample_rate, data = wavfile.read(wave_file_path) # Skips metadata
 
 print(f"sample rate = {sample_rate}")
-# print(f"number of channels = {data.shape[1]}") # assume one channel cause error 
+if len(data.shape) > 1:
+    print(f"number of channels = {data.shape[1]}") 
+    # If multiple channels, choose channel 4 (Jen's favorite)
+    data = data[:, desired_channel]  # Select first channel
+
+else:
+    print("error: number of channels must = 1")
+
+if down_sample_tf:
+    data = data[::10]                   # Take every 10th sample
+    sample_rate = sample_rate // 10     # Adjust the sample rate accordingly
+    
+
 length = data.shape[0] / sample_rate
 print(f"length = {length}")
 time = np.linspace(0, length, data.shape[0])
+
+print(data.shape)
 
 # Plot amplitude vs time
 # plt.plot(time, data, label="Left channel")
@@ -19,8 +45,15 @@ time = np.linspace(0, length, data.shape[0])
 
 # Plot spectrogram 
 f, t, spectro = signal.spectrogram(data, sample_rate)
-plt.pcolormesh(t, f, spectro, shading = 'gouaraud')
+spectro_db = 10 * np.log10(spectro + 1e-10)                             # Convert to dB scale, avoid log(0)
+plt.pcolormesh(t, f, spectro_db, shading='gouraud', cmap='magma')       # Try plotting in log scale
+# plt.pcolormesh(t, f, spectro, shading = 'gouraud')                    # normal
+
+plt.axis("off")
+plt.savefig('test1.jpeg', bbox_inches='tight', pad_inches=0)
+
+plt.axis('on')
 plt.ylabel('Frequency [Hz]')
-plt.ylim(0,1000)
+plt.ylim(0,15000)
 plt.xlabel('Time [sec]')
 plt.show()
