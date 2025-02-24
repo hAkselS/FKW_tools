4a4b8c88044e63ad40da7334511814097d8a11a2
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-02-24 10:22:06 -1000

    Able to iterate through files ina command line input directory.

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -16,5 +16,17 @@ import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_directory", help="process audio in this directory")
-parser.add_argument("-o", "--output", help="choose a location for image outputs")
+parser.add_argument("-o", "--output", help="choose a location for image outputs") # output directory 
+args = parser.parse_args()
 
+
+for i, file in enumerate(sorted(os.listdir(args.input_directory))):
+    filename = os.path.join(args.input_directory, file)
+    if os.path.isfile(filename):
+        print(f"found {filename}")
+
+
+# Debugging code 
+print(f"\nIncoming args:")
+print(f"input_directory = [{args.input_directory}]")
+print(f"output directory = [{args.output}]")
