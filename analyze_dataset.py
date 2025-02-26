2a39ec47439bb92ade3a26534584aebd22b5aeb3
Aksel Sloan <akselsloan@gmail.com>
2025-02-25 14:49:02 -1000

    Minor changes.

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -24,14 +24,19 @@ parser.add_argument("-c","--count", type=int, default = 1,
 args = parser.parse_args()
 
 audio_to_spectro_path = "audio_to_spectro.py"
-for i, file in enumerate(sorted(os.listdir(args.input_directory))):
+
+.""
+i = 0
+for  file in sorted(os.listdir(args.input_directory)):
+    '''
+    Analyze 'count' number of files in a given directory
+    '''
     filename = os.path.join(args.input_directory, file)
     if os.path.isfile(filename):
         if (i > args.count): 
             print(f"Max file analysis count [{args.count}] reached, exiting...")
             sys.exit(0)
-        
-        
+
         with open('logs/analyst_logs.csv', mode='a+', newline='') as analyst_logs: 
             analyst_logs.seek(0) # Move cursor to the start of the existing data
             reader = csv.reader(analyst_logs)
@@ -46,8 +51,7 @@ for i, file in enumerate(sorted(os.listdir(args.input_directory))):
                 writer = csv.writer(analyst_logs)
                 writer.writerow([filename])
                 print(f"Logged [{filename}] as analyzed")
-
-            
+                i = i + 1 # Only increment i if you have analyzed a file
 
         # Analyze the file 
         subprocess.run(["python3", audio_to_spectro_path, filename], stdout=sys.stdout, stderr=sys.stderr)
