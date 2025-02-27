a936f040e30f15de84a17f663d3dcf03f376bc1f
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-02-27 12:35:05 -1000

    New file.

---
+++ split_dataset.py
@@ -0,0 +1,11 @@
+'''
+File:   split_dataset.py
+
+Spec:   This program will take an directory input. The directory 
+        should be mostly filled with images, only a subset need to
+        be annotated. This program will split images by train,
+        validate, and test. Within each catagory, there will be a
+        folder for every class. 
+
+Usage:  ...
+'''
