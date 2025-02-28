005b084fed3fdb219a0ee49b67bec58537a0fdae
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-02-28 12:08:55 -1000

    Redesigned split dataset, only getting stats now.

--- split_dataset.py
+++ split_dataset.py
@@ -5,7 +5,7 @@ Spec:   This program will take an directory input. The directory
         should be mostly filled with images, only a subset need to
         be annotated. This program will split images by train,
         validate, and test. Within each catagory, there will be a
-        folder for every class. This program requires the csv option output
+        folder for every class. This program requires the csv-output
         from label studio. 
 
 Usage:  
@@ -31,39 +31,32 @@ import shutil
 # Globals --> args (TODO) 
 input_directory = '/images' # TODO: fix before use 
 input_csv = 'annotations_output.csv'
-output_directory = 'temporary_image_directory'
+output_directory = 'FKW_training_data'
 
 split_ratio = {"train": 0.7, "val": 0.2, "test": 0.1}
 catagories = ['whistle', 'click']
-
-# Get all images that have been annotated into a new directory
 
 '''
 ['annotation_id', 'annotator', 'choice', 'created_at', 'id', 'image', 'lead_time', 'updated_at']
 '''
 print(f'input directory = {input_directory}')
 
-# Make a temporary place for images WITH annotations 
-os.makedirs(output_directory, exist_ok=True)
-if os.path.exists(output_directory):
-    print(f'output directory = {output_directory}')
+ # 
+# os.makedirs(output_directory, exist_ok=True)
+# if os.path.exists(output_directory):
+#     print(f'output directory = {output_directory}')
 
-# Move files into temp directory
+# Look at the dataset, determine number of each class 
+
 df = pandas.read_csv(input_csv)
-# TODO: need to create a cascade filter here
-# if is file, if is catagory, move tmp location
-# determine counts of each class from tmp location
-# move from temp location to final file structure 
-if 'image' in df.columns:
-    for item in df['image']:
-        file_path = os.path.join(input_directory, item)
-        if os.path.isfile(file_path): 
-            # Move to tmp 
-            shutil.copy(file_path, output_directory)
-            print(f'Copied [{file_path}] to [{output_directory}]')
-        else:
-            print(f"File not found: [{file_path}]")
+
+print(df.columns)
+
+whistle_images = df.loc[df['choice'] == 'whistle', 'image'].tolist()
+click_images = df.loc[df['choice'] == 'click', 'image'].tolist()
+whistle_click_images = df.loc[df['choice'].str.contains('"whistle"' and '"click"', na=False), 'image']
 
 
-
-
+print(f'Number of whistle images: {len(whistle_images)}')
+print(f'Number of click images: {len(click_images)}')
+print(f'Number of whistle & click images: {len(whistle_click_images)}')
