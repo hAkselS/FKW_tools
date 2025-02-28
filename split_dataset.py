5c9ada3bcbd8609ef89c757ab6a4911635cb3ae2
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-02-27 14:38:29 -1000

    Setup final goal.

--- split_dataset.py
+++ split_dataset.py
@@ -33,33 +33,37 @@ input_directory = '/images' # TODO: fix before use
 input_csv = 'annotations_output.csv'
 output_directory = 'temporary_image_directory'
 
+split_ratio = {"train": 0.7, "val": 0.2, "test": 0.1}
+catagories = ['whistle', 'click']
+
 # Get all images that have been annotated into a new directory
-def copy_annotated_images_to_tmp(input_directory, input_csv):
-    '''
-    Move all images with annotations into a temp directory
-    ['annotation_id', 'annotator', 'choice', 'created_at', 'id', 'image', 'lead_time', 'updated_at']
-    '''
-    print(f'input directory = {input_directory}')
 
-    # Make a temporary place for images WITH annotations 
-    os.makedirs(output_directory, exist_ok=True)
-    if os.path.exists(output_directory):
-        print(f'output directory = {output_directory}')
-    
-    # Move files into temp directory
-    df = pandas.read_csv(input_csv)
-    if 'image' in df.columns:
-        for item in df['image']:
-            file_path = os.path.join(input_directory, item)
-            if os.path.isfile(file_path): 
-                # Move to tmp 
-                shutil.copy(file_path, output_directory)
-                print(f'Copied [{file_path}] to [{output_directory}]')
-            else:
-                print(f"File not found: [{file_path}]")
+'''
+['annotation_id', 'annotator', 'choice', 'created_at', 'id', 'image', 'lead_time', 'updated_at']
+'''
+print(f'input directory = {input_directory}')
+
+# Make a temporary place for images WITH annotations 
+os.makedirs(output_directory, exist_ok=True)
+if os.path.exists(output_directory):
+    print(f'output directory = {output_directory}')
+
+# Move files into temp directory
+df = pandas.read_csv(input_csv)
+# TODO: need to create a cascade filter here
+# if is file, if is catagory, move tmp location
+# determine counts of each class from tmp location
+# move from temp location to final file structure 
+if 'image' in df.columns:
+    for item in df['image']:
+        file_path = os.path.join(input_directory, item)
+        if os.path.isfile(file_path): 
+            # Move to tmp 
+            shutil.copy(file_path, output_directory)
+            print(f'Copied [{file_path}] to [{output_directory}]')
+        else:
+            print(f"File not found: [{file_path}]")
 
 
 
-copy_annotated_images_to_tmp(input_directory, input_csv)
 
-# TODO: consider just doing the split here. 
