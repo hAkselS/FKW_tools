f6020a48013c7be5862c305e0f7a8824bef09e49
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-06-10 16:20:49 -1000

    Turning filenames into datetimes.

--- model_training/create_pamguard_annotations.py
+++ model_training/create_pamguard_annotations.py
@@ -1,7 +1,10 @@
 '''
 File:   create_pamguard_annotatoins.py
 
-Spec:   Use this script to translate pamguard annotations to annotations usable with YOLO. 
+Spec:   This program looks at the spectrograms you have already made and assigns each one a datetime range.
+        This program then looks at existing pamguard annotations and determines if
+        the annotation falls within a time range of an existing spectrogram. If an annotation falls
+        within the time range of a spectrogram, a new annotation is created and saved as a text file. 
 
 I/O:    This program expects a CSV with the following category names: 
             "","UID","UTC","freqBeg","freqEnd","freqMean","freqStdDev","duration","freqSlopeMean","freqAbsSlopeMean","freqPosSlopeMean","freqNegSlopeMean","freqSlopeRatio","freqStepUp","freqStepDown","numSweepsDwnFlat","numSweepsDwnUp","numSweepsFlatDwn","numSweepsFlatUp","numSweepsUpDwn","numSweepsUpFlat","numInflections","freqCofm","freqQuarter1","freqQuarter2","freqQuarter3","freqSpread","freqMin","freqMax","freqRange","freqMedian","freqCenter","freqRelBw","freqMaxMinRatio","freqBegEndRatio","freqNumSteps","stepDur","freqBegSweep","freqBegUp","freqBegDwn","freqEndSweep","freqEndUp","freqEndDwn","freqSweepUpPercent","freqSweepDwnPercent","freqSweepFlatPercent","inflMaxDelta","inflMinDelta","inflMaxMinDelta","inflMeanDelta","inflStdDevDelta","inflMedianDelta","inflDur","BinaryFile","eventId","detectorName","db","species"
@@ -9,11 +12,14 @@ I/O:    This program expects a CSV with the following category names:
         via exporting cruise data from R. 
 
 Note:   The last three digits is the AC number. Ex: 1705109 is LaskerAC109
-Usage:  python3 model_training/create_pamguard_annotations.py ... 
+Usage:  python3 model_training/create_pamguard_annotations.py <folder/with/spectrograms> <path/to/csv>
 '''
 
 import pandas as pd 
 import argparse
+from datetime import datetime, timedelta
+import os
+
 
 ###################################################################
 # CONFIGURATION DEFAULTS
@@ -23,29 +29,50 @@ cruise_numbers = [1705, 1706] # Lasker, Sette
 
 # Accept command line args
 parser = argparse.ArgumentParser()
-parser.add_argument("csv_file_path", help="this csv file should contain vocalization localizations.")
+parser.add_argument("spectrogram_folder", help="this folder should contain the spectrograms you have already made.")
+parser.add_argument("csv_filepath", help="this csv file should contain vocalization localizations.")
 args = parser.parse_args()
 
-# Helper Function
-def utc_format_transformer(input_time):
-    ''' 
-    Take an input of the form: 2017-08-18 13:39:03.945 (as in csv)
-    and transform to: 1706_20170715_220250_436 (how wave files are saved)
-    '''
-    date_part, time_part = input_time.split()
-    date_formatted = date_part.replace("-", "")
-    time_main, millis = time_part.split(".")
-    time_formatted = time_main.replace(":", "")
-    return f"{date_formatted}_{time_formatted}_{millis}"
+# Turn args into variables (style points)
+spectrogram_folder = args.spectrogram_folder 
+csv_filepath = args.csv_filepath
+
+## HELPER FUNCTIONS 
+# Translate file names to times 
+def file_name_to_time(file_name):
+    # Remove prefix and suffix
+    base = file_name.split('-')[0]  # '1706_20170709_034442_942'
+    parts = base.split('_')  # ['1706', '20170709', '034442', '942']
+    
+    date_part = parts[1]  # '20170709'
+    time_part = parts[2]  # '034442'
+
+    # Combine into datetime object
+    dt = datetime.strptime(f"{date_part}_{time_part}", "%Y%m%d_%H%M%S")
+
+    # Check if '-0011' is in the filename and add 30 seconds if so
+    if '-0011' in file_name:
+        dt += timedelta(seconds=30)
+
+    return dt
+
+
+# Get datetimes from existing spectrograms
+file_and_datatime = []
+def find_spectro_times(path_to_spectros):
+    ''''tbd'''
+    file_names = os.listdir(path_to_spectros)
+    file_names = [f for f in file_names if os.path.isfile(os.path.join(path_to_spectros,f))]
+
+    for i in file_names: 
+        time = file_name_to_time(i)
+        entry = [i, time]
+        file_and_datatime.append(entry)
+
 
 # Load CSV using pandas
-df = pd.read_csv(args.csv_file_path)
+df = pd.read_csv(args.csv_filepath)
 
-# Access second row's 'utc' column
-if 'UTC' in df.columns and len(df) > 1:
-    test1 = df.loc[4120, 'UTC']
-    print(utc_format_transformer(test1))
-else:
-    print("Column 'utc' not found or insufficient rows.")
+find_spectro_times(spectrogram_folder)
 
-
+print(file_and_datatime)
