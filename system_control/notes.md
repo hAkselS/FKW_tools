bfc64f8c95bde31410f1989759243f30f0b7a7c0
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-07-22 15:22:35 -1000

    Figured out startup process.

---
+++ system_control/notes.md
@@ -0,0 +1,7 @@
+### Notes
+
+Use <contrab -e>
+
+add <@reboot /path/to/write_hello.sh>
+
+so that hello world runs on boot! 
--- system_control/start.sh
+++ system_control/start.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
 # Start the main python scirpt that runs FKW analysis on the glider.
 
+output_file=~/hello.txt
+
+echo "hello world" > "$output_file"
+
+# This is temp to show the process
